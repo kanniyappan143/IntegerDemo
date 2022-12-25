2d4cb02d855a22e2c93275784eabc427f19cc39e
kanniyappan143 <119713241+kanniyappan143@users.noreply.github.com>
2022-12-25 20:38:09 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,52 @@
+package basic;
+
+public class Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//integer sample method implement (jk)
+
+		byte a=27;
+		byte b=67;
+			System.out.println("sample a value a is "+a);
+			System.out.println("Sample value b is "+b);
+		
+		int c=123;
+		int d=1234;
+		int e=12345;
+			System.out.println("Integer entered c value is "+c);
+			System.out.println("Integer entered d value is"+d);
+			System.out.println("Integer entered evalue is"+e);
+		//float and double example implement (jk)
+			//is res decimal value only but you can assign any integer value into double
+			// it always stores like decimal value at the given memory place always
+			
+		float f=1;
+		float g=2;
+			System.out.println("Float value always output in point number is  "+f);
+			System.out.println("Float value always output in point number is "+g);
+				//double is res decimal value only but you can assign any integer value into double
+							// it always stores like decimal value at the given memory placealways sto 
+		double h=3;
+		double i=4;
+		 	System.out.println("Double value  is also output in point number"+h);
+		 	System.out.println("Double value is also print on point number"+i);
+		 	
+		//character example implement 
+		 	//char Stores only one letter within a single codes
+		 	//Example
+		 	//  'a'
+		 	char jk= 's';
+		 		System.out.println("Example char iplement is "+jk);
+		
+		 //boolean example implement 	
+		 		//boolean  data type stores true or false value only 
+		 		boolean m=true;
+		 		System.out.println("example implement is "+m);
+		 
+		 	
+		 	
+		 	
+	}
+
+}
